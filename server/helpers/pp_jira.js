717e37632b13f510c810a8a1c8d4f2c88a731cbb
Adam Conway <adam@conways.name>
2015-07-03 20:32:10 +0300

    Fixed basic infrastructure for connection to Jira

--- server/helpers/pp_jira.js
+++ server/helpers/pp_jira.js
@@ -12,19 +12,45 @@ var logger = new PP_Logger;
  * @constructor
  */
 function PP_Jira(){
-	//this._request = require('request');
+	// Set base URL
 	this._jira_base_url = '' + settings.jira_url;
+	// Ensure ends in slash
 	if( this._jira_base_url.slice(-1) !== '/' ){
 		this._jira_base_url += '/';
 	}
+	// Set link URL
 	this._jira_link_url = this._jira_base_url ? this._jira_base_url + 'browse/' : false;
+
+	// Create request object with default settings
 	this._request = require('request').defaults({
-		'baseUrl': this._jira_base_url + 'rest/api/2/',
-		'json': true
-     });
+		baseUrl: this._jira_base_url + 'rest/api/2/',
+		json: true,
+        jar: true
+    });
 
-
+	this._username = false;
+	this._dispname = false;
+	this._post_callback = false;
+	this._request_in_progress = false;
+	this._message = '';
+	this._logged_in = false;
 }
+
+PP_Jira.prototype.get_username = function(){
+	return this._username;
+};
+PP_Jira.prototype.get_dispname = function(){
+	return this._dispname;
+};
+PP_Jira.prototype.is_logged_in = function(){
+	return this._logged_in;
+};
+PP_Jira.prototype.get_message = function(){
+	return this._message;
+};
+PP_Jira.prototype.is_request_in_progress = function(){
+	return this._request_in_progress;
+};
 
 /**
  * Gets the URL for linking to a Jira issue
@@ -45,25 +71,92 @@ PP_Jira.prototype.get_jira_link_url = function(issue_id){
  * @param callback function Callback function to process data
  * @private
  */
-PP_Jira.prototype._get = function(service, data, username, password, callback) {
-	var url = this._jira_base_url + 'rest/api/2/' + service;
-	//var auth = {
-	//	'user': username,
-	//	'pass': password,
-	//	'sendImmediately': true
-	//};
-	var auth = 'Basic ' + new Buffer(username + ':' + password ).toString('base64');
-	logger.log(url);
-	logger.log(auth);
-	this._request({
+PP_Jira.prototype._get = function(service, data, callback, username, password) {
+	// Options for request
+	var opts = {
 		url: '/' + service,
-		method: 'GET',
-		qs: data,
-		headers: {
-			'Authorization': auth
-		}
+		method: 'GET'
+	};
 
-    }, callback);
+	// If we have data add to the request
+	if( data ) {
+		opts.qs = data;
+	}
+
+	// If we have a username then log in.  Otherwise assume a session
+	if( username ){
+		opts.auth = {
+			'user': username,
+			'pass': password,
+			'sendImmediately': true
+		};
+	}
+
+	// Save this as a variable then wrap callback in an anonymous function in order to add the jira param
+	var jira = this;
+
+	// Run request
+	this._request_in_progress = true;
+	this._request(opts, function(error, response, body){
+		callback.call(this, error, response, body, jira);
+	});
+};
+
+/**
+ * Authenticates the user and creates a session
+ *
+ * @param username string
+ * @param password string
+ */
+PP_Jira.prototype.authenticate = function(username, password, callback){
+	this._post_callback = callback;
+	this._username = username;
+
+	// Run request
+	this._get( 'myself', false, this._authentication_callback, username, password);
+};
+
+/**
+ * Callback run when authentication response is received.
+ *
+ * Note: This is a callback, so this inside this function refers to the *request* object,
+ * not the PP_Jira object.  The PP_Jira object is passed in as a function.
+ *
+ * @param error {} Error object from request
+ * @param response http.IncomingMessage response object
+ * @param body {} The body object returned
+ * @param jira PP_Jira the calling object
+ * @private
+ */
+PP_Jira.prototype._authentication_callback = function(error, response, body, jira){
+	jira._logged_in = false;
+	jira._dispname = false;
+	switch( response.statusCode ){
+		case 200:
+			if( body.active ){
+				jira._logged_in = true;
+				jira._message = 'Login Succeeded';
+				jira._dispname = body.displayName;
+			}
+			break;
+		case 401:
+			jira._message = 'Login Failed';
+			break;
+		case 403:
+			jira._message = 'Login attempt refused';
+			break;
+		case 404:
+			jira._message = 'Authentication succeeded but user not found!';
+			break;
+		default:
+			jira._message = 'Login Failed with code ' + response.statusCode + ' ' + response.statusText;
+			break;
+	}
+
+	if( typeof jira._post_callback === 'function' ) {
+		jira._post_callback(jira, body);
+		jira._post_callback = false;
+	}
 };
 
 /**
@@ -74,8 +167,8 @@ PP_Jira.prototype._get = function(service, data, username, password, callback) {
  * @param password string Password for authentication
  * @param callback function Callback function to process data
  */
-PP_Jira.prototype.get_issue = function(issue_id, data, username, password, callback){
-	this._get('issue/' + issue_id, data, username, password, callback);
+PP_Jira.prototype.get_issue = function(issue_id, data, callback){
+	this._get('issue/' + issue_id, data, callback, false, false);
 };
 
 
--- server/poker.js
+++ server/poker.js
@@ -13,60 +13,22 @@ var logger = new PP_Logger;
 var PP_Jira = require('./helpers/pp_jira').PP_Jira;
 var jira = new PP_Jira;
 
-var https = require('https');
-/*
-var auth = 'Basic ' + new Buffer(username + ':' + password ).toString('base64');
-logger.log(auth);
-var options = {
-	host: 'jira.carus.com',
-	path: '/rest/api/2/issue/WEB-301',
-	headers: {
-		'Authorization': auth
+jira.authenticate('', '', function(jira){
+	if( jira.is_logged_in() ){
+		jira.get_issue('WEB-301', {'fields': 'summary'}, function(error, response, body, jira){
+			logger.log('');
+			logger.log('got issue');
+			logger.log(jira.get_dispname());
+			logger.log('response');
+			logger.log(response.statusCode);
+			logger.log('body');
+			logger.log_o(body, 3);
+			logger.log('');
+		});
+
 	}
-};
-var callback = function(response){
-	var body = '';
-	//another chunk of data has been recieved, so append it to `str`
-	response.on('data', function (chunk) {
-		body += chunk;
-	});
-
-	//the whole response has been recieved, so we just print it out here
-	response.on('end', function () {
-		logger.log(body);
-	});
-};
-https.request(options, callback).end();
-*/
-
-
-
-/*
-jira.get_issue('WEB-301', {'fields': 'summary'}, '', 'foo', function(error, response, body){
-	logger.log('');
-	logger.log('wrong password');
-	logger.log('error');
-	logger.log_o(error, 3);
-	logger.log('response');
-	logger.log(response.statusCode);
-	logger.log('body');
-	logger.log_o(body, 3);
-	logger.log('wrong password done');
-	logger.log('');
 });
-jira.get_issue('WEB-301', {'fields': 'summary'}, '', '', function(error, response, body){
-	logger.log('');
-	logger.log('correct password');
-	logger.log('error');
-	logger.log_o(error, 3);
-	logger.log('response');
-	logger.log(response.statusCode);
-	logger.log('body');
-	logger.log_o(body, 3);
-	logger.log('correct password done');
-	logger.log('');
-});
-*/
+
 
 // Get and setup WebSocketServer
 var WebSocketServer = require( 'ws' ).Server;
